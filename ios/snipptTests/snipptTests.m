290225b8d49f1344d1557ccc5e9853f290b425ce
Alican <alican.torun@cirplus.io>
2020-04-22 16:52:51 +0200

    rename snippt

--- ios/snipptTests/snipptTests.m
+++ ios/snipptTests/snipptTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mindCastV3Tests : XCTestCase
+@interface snipptTests : XCTestCase
 
 @end
 
-@implementation mindCastV3Tests
+@implementation snipptTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
